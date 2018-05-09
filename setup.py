5842b34d16c3e26ed1d00afc5023e701e605cb76
Jack Ogaja <jack.ogaja@gmail.com>
2018-05-09 11:12:44 -0400

    updates

--- setup.py
+++ setup.py
@@ -94,11 +94,7 @@ setup(
         'Operating System :: macOS',
         'Operating System :: POSIX',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
 
